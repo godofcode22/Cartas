20708b3068ffe935c562fae2e38896ad2d59cbc7
godofcode22 <demollinedo@gmail.com>
2025-11-05 07:18:05 -0600

    correcion interface

--- Clases/Class1.cs
+++ Clases/Class1.cs
@@ -2,16 +2,8 @@ using System;
 
 
 
-public class Class1: Interface1
+public class Class1
 {
-    public static void Saludar()
-    {
-        Console.WriteLine("Hola desde Class1");
-    }
-    public void Metodo1()
-    {
-        Console.WriteLine("Implementación de Metodo1 en Class1");
-    }
-    
+
 
 }
